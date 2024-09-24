02d3ce60bf30fa1e63a34487aa7a98a76f45fb16
pratap-sharepoint <pratap.bangosavi84@gmail.com>
2024-09-24 15:11:30 +0530

    name change

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
-import State from './context/State.jsx'
+import State from './context/AuthState.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
